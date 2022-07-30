8f71a1bec04a136aa8ed2dd923ccb64096b2e24e
Sarah <happylucylocket123@hotmail.com>
2022-07-30 00:21:24 -0700

    Add constructors for the states

--- src/state/GameEndState.java
+++ src/state/GameEndState.java
@@ -3,6 +3,11 @@ package state;
 import java.awt.*;
 
 public class GameEndState implements StateInterface {
+    private static GameEndState instance;
+    public static GameEndState instance() {
+        if (instance == null) instance = new GameEndState();
+        return instance;
+    }
     @Override
     public void render(Graphics g) {
 
--- src/state/MenuState.java
+++ src/state/MenuState.java
@@ -3,13 +3,18 @@ package state;
 import java.awt.*;
 
 public class MenuState implements StateInterface {
+    private static MenuState instance;
+    public static MenuState instance() {
+        if (instance == null) instance = new MenuState();
+        return instance;
+    }
     @Override
     public void render(Graphics g) {
-
     }
 
     @Override
     public void tick() {
+        // Check for player clicking Start
 
     }
 }
