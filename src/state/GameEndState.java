8e3f9322cf0f1c6331a849fd5207e47291c65111
Sarah <happylucylocket123@hotmail.com>
2022-07-29 23:47:22 -0700

    Add GameEnd and Menus states to implement later.

---
+++ src/state/GameEndState.java
@@ -0,0 +1,15 @@
+package state;
+
+import java.awt.*;
+
+public class GameEndState implements StateInterface {
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+}
---
+++ src/state/MenuState.java
@@ -0,0 +1,15 @@
+package state;
+
+import java.awt.*;
+
+public class MenuState implements StateInterface {
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+}
